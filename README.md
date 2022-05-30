de922c984404bfe7a98d0de45a92b328d525f557
yugmani <yugmani@yahoo.com>
2022-05-30 00:31:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-xed2gj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-xed2gj)
---
+++ index.js
@@ -0,0 +1,22 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+// *********************************************
+// **************** Synchronous ****************
+// *********************************************
+
+function A() {
+  console.log('A');
+}
+
+function B() {
+  console.log('B');
+}
+
+A();
+B();
+
+//A
+//B
