61ebd2527e5b2d276c3abe458eab90172ab0cae0
yugmani <yugmani@yahoo.com>
2022-05-31 13:33:04 -0700

    callback and examples

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
 
+import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+
 // Write Javascript code!
 
 // *********************************************
@@ -74,3 +76,40 @@ function namaste() {
 // }, 1000);
 
 // clearInterval(clearTimer);
+
+// **************************************************
+// **************** Callbacks *******************
+// **************************************************
+
+//examples of callbacks
+
+//example 1
+function sayName(name) {
+  console.log(`Welcome to Los Angeles, ${name}!`);
+}
+
+function greetSomeone(greetFunction) {
+  const name = 'Yoog';
+  greetFunction(name);
+}
+
+greetSomeone(sayName);
+// Welcome to Los Angeles, Yoog! -> logged.
+// Here the function 'greetSomeone' takes another function 'sayName' as an argument.
+// So, 'sayName' is callback function and 'greetSomeone' is considered as higher order function.
+
+//example 2
+function callback() {
+  document.getElementById('demo').innerHTML = 'Hello World';
+}
+
+document.getElementById('btn').addEventListener('click', callback);
+//the callback function is executed only when the user clicks on the button.
+
+//example 3
+$.get('url', function (data) {
+  $('.result').html(data);
+  console.log('Load was performed...');
+});
+
+// The callback is executed only after the data fetching is over.
